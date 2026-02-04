949ff7ce27250594219a29f319239947224f3dfe
MLandis-4 <max.landis@student.northampton.edu>
2026-02-04 19:48:38 +0000

    I created a program that uses in/outdata streams to select a file then read the contents, comupting an employee's total revenue.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
+//Max Landis
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  
+  ifstream inData;
+  
+  string firstName, lastName;
+  string filename;
+  double baseSalary, comissionPercent, totalSales, employeeExpenses, comission, total;
+
+  cout << "Which filename would you like to open?" << endl;
+  cin >> filename >> endl;
+  inData.open(filename);
+
+  cout << fixed << setprecision(2);
+
+  inData >> firstName >> lastName >> baseSalary >> comissionPercent >> totalSales >> employeeExpenses;
+
+  comission = (totalSales*comissionPercent)/100;
+
+  total = baseSalary+comission+employeeExpenses;
+
+  cout << "Payroll data for" << " " << firstName << " " << lastName << endl;
+  cout << " " << endl;
+  cout << "Base Salary: " << setw(12) << baseSalary << endl;
+  cout << "Comission: " << setw(14) << comission << endl;
+  cout << "Expenses: " << setw(15) << employeeExpenses << endl;
+  cout << setw(25) << "--------" << endl;
+  cout << "Total: " << setw(18) << total << endl;
+
 }
